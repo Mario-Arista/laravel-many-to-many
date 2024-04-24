828c9c2764a902eefe918b13786330eb82adde24
mario.arista <mario.arista.dev@gmail.com>
2024-04-24 15:47:03 +0200

    comand: php artisan make:model Technology -rcms --requests + Tecnologyseeder + change color column in Technologies

--- database/migrations/2024_04_24_132641_create_technologies_table.php
+++ database/migrations/2024_04_24_132641_create_technologies_table.php
@@ -13,9 +13,9 @@ return new class extends Migration
     {
         Schema::create('technologies', function (Blueprint $table) {
             $table->id();
-            
+
             $table->string('name', 20);
-            $table->string('color', 7);
+            $table->string('color', 7)->nullable();
 
             $table->timestamps();
         });
